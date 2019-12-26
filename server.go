5ab8cd8c855aa4a275a0b4a3eb9b0b48297c9f69
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-26 13:48:05 +0900

    update: 構造体の追加

--- server.go
+++ server.go
@@ -5,10 +5,18 @@ import(
     "github.com/labstack/echo"
 )
 
+type User struct {
+    ID int `json:"id"`
+    Name string `json:"name"`
+    Email string `json:email`
+}
+
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+    e.GET("/", hello)
     e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello")
 }
